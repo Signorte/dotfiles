84ed83061f4e4abc58213793361238af4734077d
tsukunii <cogito.ergo.sumirus@gmail.com>
2024-03-01 10:30:26 +0900

    Update initial_install.sh

--- initial_install.sh
+++ initial_install.sh
@@ -1,3 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install --cask slack
 brew install --cask cursor
+brew install --cask karabiner-elements
