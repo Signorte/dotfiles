60faa22e6f7bb38235474d97d5d7fd3a87149fd7
tsukunii <cogito.ergo.sumirus@gmail.com>
2024-03-08 13:52:56 +0900

    Update initial_install.sh

--- initial_install.sh
+++ initial_install.sh
@@ -1,4 +1,11 @@
+# install HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install apps
 brew install --cask slack
 brew install --cask cursor
 brew install --cask karabiner-elements
+brew install --cask zoom
+
+# install optional apps (comment-in to use)
+# brew install --cask snagit
